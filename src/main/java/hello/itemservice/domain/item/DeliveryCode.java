aca9cdcae780f956a9a2e7a719c53ca8d52ab21f
superssol <hansole10@gmail.com>
2022-12-21 20:01:57 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -0,0 +1,19 @@
+package hello.itemservice.domain.item;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+/**
+ * FAST : 빠른배송
+ * NORMAL : 일반 배송
+ * SLOW : 느린배송
+ */
+@Data
+@AllArgsConstructor
+public class DeliveryCode {
+
+    private String code;
+    private String displayName;
+
+}
